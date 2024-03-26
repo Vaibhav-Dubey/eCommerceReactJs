deefebe3140ec6fe58c705197f40b47f28d6dfdc
Vaibhav Dubey <56965007+Vaibhav-Dubey@users.noreply.github.com>
2024-03-26 06:56:25 -0700

    finished site with some changes left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+// react router v6
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// pages
+import {Home, CategoryProduct, ProductSingle, Cart, Search} from "./pages/index";
+// components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+import store from "./store/store";
+import {Provider} from "react-redux";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store = {store}>
+        <BrowserRouter>
+          <Header />
+          <Sidebar />
+
+          <Routes>
+            {/* home page route */}
+            <Route path = "/" element = {<Home />} />
+            {/* single product route */}
+            <Route path = "/product/:id" element = {<ProductSingle />} />
+            {/* category wise product listing route */}
+            <Route path = "/category/:category" element = {<CategoryProduct />} />
+            {/* cart */}
+            <Route path = "/cart" element = {<Cart />} />
+            {/* searched products */}
+            <Route path = "/search/:searchTerm" element = {<Search />} />
+          </Routes>
+
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
