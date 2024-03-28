c073e748a9972d9b3c5e51d59d061ee0d8524d40
Vaibhav Dubey <56965007+Vaibhav-Dubey@users.noreply.github.com>
2024-03-28 12:55:24 -0700

    changes to the slider

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,5 +1,5 @@
 import slider_img_1 from "../assets/images/slider_img_1.jpg";
-import slider_img_2 from "../assets/images/slider_img_2.jpg";
+import slider_img_2 from "../assets/images/slider_img_2.png";
 import shopping_cart from "../assets/images/shopping_cart.png";
 import correct from "../assets/images/correct.png";
 import loader from "../assets/images/loader.svg";
